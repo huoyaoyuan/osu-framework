250ab6509f9868d2f84b662100f3b670ebbce3d2
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2023-05-28 22:28:29 +0800

    Use ClangSharpPInvokeGenerator to generate bindings for libavutil.

---
+++ osu.Framework/Graphics/Video/FFmpeg/NativeTypeName.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Diagnostics;
+
+namespace osu.Framework.Graphics.Video.FFmpeg
+{
+
+    [Conditional("DEBUG")]
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple = false, Inherited = true)]
+    internal sealed class NativeTypeNameAttribute : Attribute
+    {
+        public NativeTypeNameAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+
+}
